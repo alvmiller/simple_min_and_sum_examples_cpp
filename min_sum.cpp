94d75ddf5c7a38119f8248baf83ca1b9930eb508
alvmiller <134068320+alvmiller@users.noreply.github.com>
2023-11-17 16:19:36 +0200

    Create min_sum.cpp

---
+++ min_sum.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <cassert>
+#include <utility>
+
+// https://godbolt.org/z/eTbsxcq96
+
+// -----------------------------------------------------------------------------
+
+template<typename T>
+constexpr auto min(T&& arg)
+{
+    return std::forward<T>(arg);
+}
+
+template<typename T, typename ... Ts>
+constexpr auto min(T&& arg, Ts&& ... args)
+{
+    return std::min(std::forward<T>(arg), min(std::forward<Ts>(args)...));
+}
+
+// -----------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------
+
+constexpr auto sum()
+{
+    return 0;
+}
+
+template <typename T, typename ... Ts>
+constexpr auto sum(T&& arg, Ts&& ... args) {
+    return arg + sum(std::forward<Ts>(args)...);
+}
+
+// -----------------------------------------------------------------------------
+
+int main()
+{
+    // -------------------------------------------------------------------------
+
+    static_assert(min(3, 1, 2) == 1, "Bad minimum result"); // CT check
+    assert(min(3, 1, 2) == 1); // RT(dbg) check
+
+    // -------------------------------------------------------------------------
+
+    static_assert(sum(1, 2, 3) == 6, "Bad compile time summary"); // CT check
+    assert(sum(1, 2, 3) == 6); // RT(dbg) check
+
+    // -------------------------------------------------------------------------
+
+    return 0;
+}
